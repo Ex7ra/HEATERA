56a2d4e485bd12434b2d5b40298b0a6f8b8c036a
Ex7ra <mikhail.shypilov@gmail.com>
2026-06-05 13:11:11 +1000

    +0.0.7

--- Assets/Scripts/MenuStaff/MainMenu.cs
+++ Assets/Scripts/MenuStaff/MainMenu.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public GameObject FSPanel;
+    public GameObject NatoPanel;
     void Start()
     {
         FSPanel.SetActive(false);
+        NatoPanel.SetActive(false);
     }
-    public void OnPoinetrDown()
+    public void OpenFSPanel()
     {
         FSPanel.SetActive(!FSPanel.activeSelf);
     }
@@ -20,6 +22,14 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quit the game");
+    }
+    public void OpenNatoPanel()
+    {
+       NatoPanel.SetActive(!NatoPanel.activeSelf); 
+    }
+    public void GoBack()
+    {
+        NatoPanel.SetActive(false);
     }
 
 }
