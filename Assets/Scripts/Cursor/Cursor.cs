3b5e15e5da082b3ad966ff25af9183be62e1466f
Ex7ra <mikhail.shypilov@gmail.com>
2026-06-11 20:52:57 +1000

    bug fix1

--- Assets/Scripts/Cursor/Cursor.cs
+++ Assets/Scripts/Cursor/Cursor.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CursorControllerComplex : MonoBehaviour
 {
@@ -6,7 +7,8 @@ public class CursorControllerComplex : MonoBehaviour
 
     [SerializeField] private Texture2D cursorTextureDefault;
     [SerializeField] private Texture2D cursorTextureTarget;
-    
+  
+    [SerializeField] private GameObject PauseMenu;
 
     private void Awake()
     {
@@ -24,6 +26,14 @@ public class CursorControllerComplex : MonoBehaviour
     void Start()
     {
         SetToMode(ModeOfCursor.Default);
+    }
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        }
+        
     }
     
     public void SetToMode(ModeOfCursor modeOfCursor)
