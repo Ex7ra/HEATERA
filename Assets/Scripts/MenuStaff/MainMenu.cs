b2592ce8405cfedeac13b61ab880be70a1eb3ec8
Ex7ra <mikhail.shypilov@gmail.com>
2026-08-04 22:13:30 +1000

    UI work 5

--- Assets/Scripts/MenuStaff/MainMenu.cs
+++ Assets/Scripts/MenuStaff/MainMenu.cs
@@ -36,6 +36,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(1);
     }
+    public void CloseTheFSPanel()
+    {
+        FSPanel.SetActive(false);
+    }
 
     public void QuitTheGame()
     {
