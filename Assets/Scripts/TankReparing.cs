ebbead9e07fa92ecb62f775194d7d4cbe530d9fd
Ex7ra <mikhail.shypilov@gmail.com>
2026-08-27 22:27:44 +1000

    reparing UI

--- Assets/Scripts/TankReparing.cs
+++ Assets/Scripts/TankReparing.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
 using System.Collections;
-using NUnit.Framework;
+using TMPro;    
+
 public class TankReparing : MonoBehaviour
 {
-    
     public TankModule[] module;
-    
+
     private TankDamageReceiver tank;
+
+    [Header("Repair UI")]
+    public GameObject repairUI;
+    public TMP_Text repairText;
+    public TMP_Text progressText;
+
+    private bool isRepairing = false;
+    private TankModule currentRepairModule;
     void Start()
     {
         module = GetComponentsInChildren<TankModule>();
@@ -16,45 +24,77 @@ public class TankReparing : MonoBehaviour
     
     void Update()
     {
-        if(tank.IsDestroyed == true)
+        if (tank.IsDestroyed)
+        {
+            repairUI.SetActive(false);
+            return;
+        }
+
+        if (isRepairing)
         {
             return;
         }
-        foreach(TankModule currentModule in module)
+
+        TankModule brokenModule = FindBrokenModule();
+
+        if (brokenModule == null)
         {
-            float healthPercent = currentModule.health / currentModule.maxHealth;
-           
-            if (healthPercent <= 0.00f)
-            {
-                Debug.Log("PRESS R TO REPAIR: " + currentModule.name);
-                if (Input.GetKeyDown(KeyCode.R))
-                {
-                    StartCoroutine(RepairModule(currentModule));
-                }
-            }
+            repairUI.SetActive(false);
+            return;
         }
-       
+
+        repairUI.SetActive(true);
+
+        repairText.text = brokenModule.name.ToUpper() + " DESTROYED\n\n" + "Press R to repair " + brokenModule.name;
+
+        progressText.text = "";
+
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            currentRepairModule = brokenModule;
+            StartCoroutine(RepairModule(currentRepairModule));
+        }
     }
 
     public IEnumerator RepairModule(TankModule currentModule)
     {
-      
+        isRepairing = true;
         float repairTime = currentModule.maxHealth / 15f;
         float time = 0f;
+
+        repairUI.SetActive(true);
+
         while (time < repairTime)
         {
             time += Time.deltaTime;
             float t = Mathf.Clamp01(time / repairTime);
-            print("Repairing " + currentModule.name + " " + (t * 100f).ToString("F2") + "%");
+
+            repairText.text = "REPAIRING " + currentModule.name.ToUpper();
+            progressText.text = "Repairing: " +(t * 100f).ToString("F0") +"%";
+
             yield return null;
         }
-        if(time >= repairTime)
+
+        currentModule.health = currentModule.maxHealth;
+        currentModule.IsDestroyed = false;
+        Debug.Log(currentModule.name + " REPAIRED");
+
+        isRepairing = false;
+    }
+    TankModule FindBrokenModule()
+    {
+        foreach (TankModule currentModule in module)
         {
-            currentModule.health = currentModule.maxHealth;
-            currentModule.IsDestroyed = false;
-            Debug.Log(currentModule.name + " REPAIRED");
-            
+            float healthPercent =
+                currentModule.health / currentModule.maxHealth;
+
+            if (healthPercent <= 0f)
+            {
+                return currentModule;
+            }
         }
-        
+
+        return null;
     }
 }
